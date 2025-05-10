0cac4c1aa9c0ebf08ba7ede1b3b7381b45b3e8fb
Alana Muller <52933853+alanamuller@users.noreply.github.com>
2025-05-09 20:10:22 -0500

    extract subject ID and eTIV from aseg.stats tried to loop through 725 subjects but only got 514 so need to troubleshoot

--- hcp_combineFreesurferValues.R
+++ hcp_combineFreesurferValues.R
@@ -1,1 +1,4 @@
-x <- 2
+#Author: Alana Muller (alana.m@wustl.edu)
+#Description: Combine freesurfer values for Human Connectome Project
+# into one excel file to do more analyses in R!
+#Date created: May 9, 2025
